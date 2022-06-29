30da55fb39ece4970da28128641d0a738ab4847a
vinade <vinade.filipe@gmail.com>
2022-06-29 00:00:37 -0300

    adjust css to mobile

--- src/styles/frame.ts
+++ src/styles/frame.ts
@@ -4,7 +4,8 @@ export default styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
-    width: 500px;
+    max-width: 500px;
+    width: 100%;
     margin: 0 auto;
     padding: 10px 0;
     background-color: ${(props) => props.theme.colors.background};
